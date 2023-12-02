496172a8dc9a1509a12abf957a69ec4015c2e3d8
Emma Waddell <52298111+reniby@users.noreply.github.com>
2023-12-02 18:04:40 -0500

    init and add header

---
+++ components/header.js
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <header>
+            <a href="/">
+                <div className="circle"></div>
+                <div className="headerTitle">SARIKA T.<br></br>DOPPALAPUDI</div>
+            </a>
+        </header>
+    )
+  }
---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import Header from '../components/header';
+import '../styles/global.css';
+
+export default function App() {
+  return (
+    <>
+        <Header />
+        <main>
+          
+        </main>
+    </>
+  )
+}
